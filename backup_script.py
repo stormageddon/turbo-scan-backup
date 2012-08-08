17ad4ac707975611873c72d1e7acc3dc2e120dd1
Mike Caputo <mpc5944@rit.edu>
2012-08-07 23:44:58 -0500

    Refactored the code into some very simple methods. Still needs more refactoring. Username and Password are now passed as command line arguments as opposed to hard-coded values.

--- backup_script.py
+++ backup_script.py
@@ -9,61 +9,75 @@
 # are then scanned for all emails sent by the TurboScan app
 # (determined by special tags in the subject line), downloaded,
 # and the image attachments are sent to a server for safe keeping.
+#
+# For more information, view the README document.
 
 import imaplib
 import email
 import os
+import sys
 
-print('Connection to GMail...')
-mail = imaplib.IMAP4_SSL('imap.gmail.com')
-mail.login('therubikscubekid@gmail.com', 'uxlkryn425')
-print('Retrieving emails...')
-mail.select('Turbo')
 
+###### Variables ######
+turbo_label = 'Turbo'
 detach_dir = '.'
+#######################
 
-typ, data = mail.search(None, 'ALL')
-for num in data[0].split():
-	typ, data = mail.fetch(num, '(RFC822)')
+## 
+#	This method attempts to make a connection to http://gmail.com using the
+#	username and password provided via command line arguments
+##
+def connect(username, password):
+	print('Connecting to GMail...')
+	mail = imaplib.IMAP4_SSL('imap.gmail.com')
+	mail.login(username, password)
+	print('Retrieving emails...')
+	mail.select(turbo_label)
+
+
+	numDownloaded = 0
+
+	typ, data = mail.search(None, 'UNSEEN')
+	for num in data[0].split():
+		typ, data = mail.fetch(num, '(RFC822)')
 	
-	newmail = email.message_from_string(data[0][1])
-	
-#	email = email.message_from_string(data[0][1])
+		newmail = email.message_from_string(data[0][1])
 
-	for part in newmail.walk():
-        	if part.get_content_maintype() == 'multipart':
-            		continue
-	        if part.get('Content-Disposition') is None:
-        		continue
-		filename = part.get_filename()
-		counter = 1
-	        if not filename:
-        		filename = 'part-%03d%s' % (counter, 'bin')
-		        counter += 1
-        	att_path = os.path.join(detach_dir, filename)
-        	if not os.path.isfile(att_path) :
-            		fp = open(att_path, 'wb')
-            		fp.write(part.get_payload(decode=True))
-            		fp.close()
+		for part in newmail.walk():
+        		if part.get_content_maintype() == 'multipart':
+            			continue
+	        	if part.get('Content-Disposition') is None:
+        			continue
+			filename = part.get_filename()
+			counter = 1
+	        	if not filename:
+        			filename = 'part-%03d%s' % (counter, 'bin')
+		        	counter += 1
+        		att_path = os.path.join(detach_dir, filename)
+        		if not os.path.isfile(att_path) :
+            			fp = open(att_path, 'wb')
+            			fp.write(part.get_payload(decode=True))
+				numDownloaded += 1
+            			fp.close()
 
+	print "Downloaded %s email attachments" % numDownloaded
 
+	mail.close()
+	mail.logout()
 
-	#print 'Message %s\n%s\n' % (num, data[0][1])
+##
+#	The main method. Accepts the command line arguments of -username -password
+#	and displays a usage statement if the wrong number of arguments is given
+##
+def main():
+	print "length: %s" % len(sys.argv)
+	if( len(sys.argv) != 3 ):
+		print "usage: turbo-scan-backup -username@gmail.com -password"
+	else:
+		username = sys.argv[1]
+		password = sys.argv[2]
+		connect(username, password)
 
-type, data = mail.fetch(1, '(RFC822)')
-#print 'Message %s\n\%s\n' % (1, email.message_from_string(data[0][1]))
-#print 'Message %s\n%s\n' % (1, data[0][1])
-
-#email = email.message_from_string(data[0][1])
-
-#print email
-
-
-
-
-
-
-#print message
-
-mail.close()
-mail.logout()
+# Runs the main() method first
+#if __name__ == "__main__":
+#    main()
